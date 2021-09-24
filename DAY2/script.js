7afa0183eb7600a1cc3cb639a787471bb36871d8
Vishal <bishalpandey2001@gmail.com>
2021-09-24 11:30:53 +0530

    day-2

---
+++ DAY2/script.js
@@ -0,0 +1,5 @@
+function animation(){
+    document.getElementById('first').classList.toggle('desk-first-active');
+    document.getElementById('second').classList.toggle('desk-second-active');
+    document.getElementById('third').classList.toggle('desk-third-active');
+}
